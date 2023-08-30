03e26f0287ce60cb824ee6d2db1134356b5cfe98
Pavel <pz@debian.ixi.int>
2023-08-30 10:48:15 +0300

    fixes in lf config, disable nvim lsp ensure_installed

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,4 @@
 require("mason").setup()
 require("mason-lspconfig").setup{
-    ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "pyright", "volar", "tsserver" },
+    -- ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "pyright", "volar", "tsserver" },
 }
