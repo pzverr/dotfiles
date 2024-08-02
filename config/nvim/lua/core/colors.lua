890902aa8e5aa46147452025981611950924e5c5
Pavel <pz@pz.ixi.int>
2024-08-02 17:38:43 +0300

    change themes for alacritty, nvim

--- config/nvim/lua/core/colors.lua
+++ config/nvim/lua/core/colors.lua
@@ -1,27 +1,27 @@
--- vim.o.termguicolors = true
+vim.o.termguicolors = true
 
--- vim.o.background = "dark"
+vim.o.background = "dark"
 
-require("onedark").setup({
-	style = "darker",
-})
-require("onedark").load()
-
--- local bg0 = "#282828"
-
--- require("gruvbox").setup({
--- 	transparent_mode = false,
--- 	overrides = {
--- 		-- SignColumn = { bg = Gruvbox.palette.dark1 },
--- 		SignColumn = { bg = bg0 },
--- 		GruvboxRedSign = { bg = bg0 },
--- 		GruvboxGreenSign = { bg = bg0 },
--- 		GruvboxYellowSign = { bg = bg0 },
--- 		GruvboxBlueSign = { bg = bg0 },
--- 		GruvboxPurpleSign = { bg = bg0 },
--- 		GruvboxAquaSign = { bg = bg0 },
--- 		GruvboxOrangeSign = { bg = bg0 },
--- 	},
+-- require("onedark").setup({
+-- 	style = "darker",
 -- })
---
--- vim.cmd([[colorscheme gruvbox]])
+-- require("onedark").load()
+
+local bg0 = "#282828"
+
+require("gruvbox").setup({
+	transparent_mode = false,
+	overrides = {
+		-- SignColumn = { bg = Gruvbox.palette.dark1 },
+		SignColumn = { bg = bg0 },
+		GruvboxRedSign = { bg = bg0 },
+		GruvboxGreenSign = { bg = bg0 },
+		GruvboxYellowSign = { bg = bg0 },
+		GruvboxBlueSign = { bg = bg0 },
+		GruvboxPurpleSign = { bg = bg0 },
+		GruvboxAquaSign = { bg = bg0 },
+		GruvboxOrangeSign = { bg = bg0 },
+	},
+})
+
+vim.cmd([[colorscheme gruvbox]])
