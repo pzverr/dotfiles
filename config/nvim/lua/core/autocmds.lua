2765a6815989cd63d4d21e38c5d2a182ac68c7ef
Pavel <pzverr@gmail.com>
2025-02-26 23:01:48 +0300

    add lua formatter; change autocmds commands

--- config/nvim/lua/core/autocmds.lua
+++ config/nvim/lua/core/autocmds.lua
@@ -5,19 +5,12 @@ augroup("SetTwoSpaceIndent", { clear = true })
 autocmd("FileType", {
 	group = "SetTwoSpaceIndent",
 	pattern = {
-		"c",
-		"cpp",
-		"cc",
-		"h",
-		"hpp",
-		"proto",
 		"js",
 		"ts",
 		"tsx",
 		"json",
 		"css",
 		"html",
-		"xml",
 		"yaml",
 	},
 	command = "setlocal shiftwidth=2 tabstop=2 softtabstop=2",
@@ -28,7 +21,20 @@ autocmd("FileType", {
 	group = "Set4TabIndent",
 	pattern = {
 		"lua",
-		"toml",
 	},
 	command = "setlocal shiftwidth=4 tabstop=4 softtabstop=4 noexpandtab",
+})
+
+-- by default
+augroup("Set4SpaceIndent", { clear = true })
+autocmd("FileType", {
+	group = "Set4SpaceIndent",
+	pattern = {
+		"c",
+		"cpp",
+		"cc",
+		"h",
+		"hpp",
+	},
+	command = "setlocal shiftwidth=4 tabstop=4 softtabstop=4 expandtab",
 })
